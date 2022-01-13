3af53aa3e93985e82f80d113384a0db0a21e716f
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-13 23:14:37 +0900

    add: Scaffold・appBar追加

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
       ),
     ),
   );
