63d6a6ec6dbabc42b37d6485983d264973f90f31
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-13 23:25:41 +0900

    add: 背景色追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+          backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
       ),
     ),
